2ca8b711cf92406e4274767ae223d403d1b3b5b2
Protap Ghosh <protapg518@gmail.com>
2023-05-29 11:57:12 +0600

    setup jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
